15a47db7878be28b599edf610cb73d399d1d71e8
steven confessore <confessore@balasolu.com>
2022-10-04 17:01:28 -0400

    diesel migrations

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -1,6 +1,20 @@
 use actix_cors::Cors;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use diesel::{pg::PgConnection, Connection};
+use diesel::{pg::{PgConnection, Pg}, Connection};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations/");
+
+
+fn run_migrations(connection: &mut impl MigrationHarness<Pg>) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+
+    // This will run the necessary migrations.
+    //
+    // See the documentation for `MigrationHarness` for
+    // all available methods.
+    connection.run_pending_migrations(MIGRATIONS)?;
+
+    Ok(())
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -20,9 +34,9 @@ async fn manual_hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     let path = std::env::var("APPLICATION__POSTGRESOPTIONS__CONNECTIONSTRING").expect("Expected a token in the environment");
     let connection_string = std::fs::read_to_string(path).expect("should have been a file path");
-    PgConnection::establish(&connection_string)
+    let mut connection = PgConnection::establish(&connection_string)
         .unwrap_or_else(|_| panic!("Error connecting to {}", connection_string));
-   
+    let _result = run_migrations(&mut connection).unwrap_or_else(|_| panic!("error running migrations"));
     HttpServer::new(|| {
         let cors = Cors::default().allow_any_origin().supports_credentials();
         App::new()
