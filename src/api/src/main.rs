94314faa5406ff2e3539e02d278a6d1017ef6269
steven confessore <confessore@balasolu.com>
2022-10-04 16:31:00 -0400

    libpq for diesel

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -1,7 +1,6 @@
-pub mod models;
-pub mod schema;
 use actix_cors::Cors;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use diesel::{pg::PgConnection, Connection};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -19,6 +18,11 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let path = std::env::var("APPLICATION__POSTGRESOPTIONS__CONNECTIONSTRING").expect("Expected a token in the environment");
+    let connection_string = std::fs::read_to_string(path).expect("should have been a file path");
+    PgConnection::establish(&connection_string)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", connection_string));
+   
     HttpServer::new(|| {
         let cors = Cors::default().allow_any_origin().supports_credentials();
         App::new()
